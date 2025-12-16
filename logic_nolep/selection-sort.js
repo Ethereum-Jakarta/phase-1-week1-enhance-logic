3713f6413a8491e62aa3dceda7c6a2a44b2e8f84
bluehoneygee <bluehoneygee@gmail.com>
2025-12-16 11:56:51 +0700

    submit sort-algo

---
+++ logic_nolep/selection-sort.js
@@ -0,0 +1,69 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+const selectionSortString = (str) => {
+  let arr = str.split("");
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+
+  return arr.join("");
+};
+
+const groupAnagrams = function (strs) {
+  let map = {};
+
+  for (let word of strs) {
+    let sortedWord = selectionSortString(word);
+
+    if (!map[sortedWord]) {
+      map[sortedWord] = [];
+    }
+
+    map[sortedWord].push(word);
+  }
+
+  return Object.values(map);
+};
+
+// Test Case 1
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+// Test Case 2
+console.log(groupAnagrams([""]));
+// Output: [[""]]
+
+// Test Case 3
+console.log(groupAnagrams(["a"]));
+// Output: [["a"]]
+
+// Test Case 4
+console.log(groupAnagrams(["listen", "silent", "hello", "world"]));
+// Output: [["listen","silent"],["hello"],["world"]]
+
+// Test Case 5
+console.log(groupAnagrams(["rat", "tar", "art", "car"]));
+// Output: [["rat","tar","art"],["car"]]
+
+// Test Case 6
+console.log(groupAnagrams(["apple", "banana", "leapp", "grape", "orange"]));
+// Output: [["apple","leapp"],["banana"],["grape"],["orange"]]
+
+// Test Case 7
+console.log(groupAnagrams(["abcd", "dcba", "xyz", "zyx", "wxyz"]));
+// Output: [["abcd","dcba"],["xyz","zyx"],["wxyz"]]
